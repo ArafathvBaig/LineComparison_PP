2d7b02774307f4fc5121faa59d6c166b8a73bad8
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-20 12:12:54 +0530

    Adding Welcome Program

---
+++ src/com/LineComparison/WelcomeProgram.java
@@ -0,0 +1,10 @@
+package com.LineComparison;
+
+public class WelcomeProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
